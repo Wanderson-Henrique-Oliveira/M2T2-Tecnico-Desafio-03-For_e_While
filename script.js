281dcbe35133623477c01155d314f519d825b1f2
Henrique Oliveira <wandholiveira1@gmail.com>
2022-06-15 01:43:43 -0300

    Exercícios While e For

---
+++ script.js
@@ -0,0 +1,40 @@
+// Exercício 01:
+
+let counterC = 1;
+console.log("Contador Crescente:")
+
+for (counterC = 1; counterC <= 10; counterC ++){
+    console.log(counterC)
+}
+
+// Exercício 02:
+
+let counterD = 10;
+console.log("Contador Decrescente:")
+
+while (counterD >= 1){
+    console.log(counterD --)
+}
+// Exercício 03:
+
+let counterI = 1;
+console.log("Contador Ímpar:")
+
+for (counterI = 1; counterI <=100; counterI +=2){
+    console.log(counterI)
+}
+
+// Exercício 04:
+
+let counterP = 0;
+console.log("Contador Par:")
+
+for (counterP = 0; counterP <=100; counterP +=2){
+    console.log(counterP)
+
+//Nesse outro caso, suprime-se o número 100 colocando
+// ele como resultado da condição 98 += 2;
+
+// while (counterP <= 98){
+//     console.log(counterP +=2)
+}
